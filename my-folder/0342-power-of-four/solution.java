9fe8ecf8fd6565726b9813c0bfbf33c5ee9aaa38
Bfacter <bhawanshbaleja@gmail.com>
2024-02-05 11:14:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.1 MB (13.04%)

---
+++ my-folder/0342-power-of-four/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n==1){
+            return true;
+        }
+        if(n==0){
+            return false;
+        }
+        int x = (int) (Math.log(n) / Math.log(4));
+        if(Math.pow(4,x)==n){
+            return true;
+        }
+       return false;
+    }
+}
