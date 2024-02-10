79ba27f8a9ee3ce26d1f465a3890ce946a5552b0
Bfacter <bhawanshbaleja@gmail.com>
2024-02-10 16:48:48 +0000

    Sync LeetCode submission - Fibonacci Number (java)

--- LeetCode/problems/fibonacci_number/solution.java
+++ LeetCode/problems/fibonacci_number/solution.java
@@ -1,19 +1,12 @@
 class Solution {
     public int fib(int n) {
-        int [] dp = new int[n+1];
-		Arrays.fill(dp, -1);
-        return fibinaccci(n,dp);
+     int[]dp=new int[n+1];
+    //  Arrays.fill(dp,-1);
+     
+       if(n<2){
+           return n;
+       }
+       dp[n]=fib(n-1)+fib(n-2);
+       return dp[n];
     }
-    static int fibinaccci(int n,int[]dp) {
-		if(n==0) {
-			return 0;
-		}if(n==1) {
-			return 1;
-		}
-		if(dp[n]!=-1) {
-			return dp[n];
-		}
-		dp[n]=fibinaccci(n-1,dp)+fibinaccci(n-2,dp);
-		return dp[n];
-	}
 }
