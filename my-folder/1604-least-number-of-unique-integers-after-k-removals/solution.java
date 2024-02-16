478de9d9428d5bee9b8b58516f31f70b8c191d60
Bfacter <bhawanshbaleja@gmail.com>
2024-02-16 15:55:58 +0000

    [LeetCode Sync] Runtime - 50 ms (57.13%), Memory - 63.2 MB (17.26%)

---
+++ my-folder/1604-least-number-of-unique-integers-after-k-removals/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        Map<Integer,Integer> mp = new HashMap<>();
+        for (int n : arr) {
+            mp.put(n, mp.getOrDefault(n, 0) + 1);
+        }
+        List<Integer> values = new ArrayList<>(mp.values());
+        Collections.sort(values);
+        int count = mp.size();
+        for (int value : values) {
+            if (k >= value) {
+                k -= value;
+                count--;
+            } else {
+                break;
+            }
+        }
+
+        return count;
+
+    }
+}
