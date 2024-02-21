8713634ff54eaf33b3ff83bdb2de82496826224e
Bfacter <bhawanshbaleja@gmail.com>
2024-02-21 14:35:27 +0000

    [LeetCode Sync] Runtime - 2 ms (90.59%), Memory - 41.3 MB (50.46%)

---
+++ my-folder/0038-count-and-say/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String countAndSay(int n) {
+        if(n<2){
+            return "1";
+        }
+        String s=countAndSay(n-1);
+        StringBuilder sb=new StringBuilder();
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            count++;
+            if(i==s.length()-1||s.charAt(i)!=s.charAt(i+1)){
+                sb.append(count).append(s.charAt(i));
+                count=0;
+            }
+
+        }
+        return sb.toString();
+     
+
+     
+    }
+}
