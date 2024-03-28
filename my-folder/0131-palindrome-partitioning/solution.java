4d5ee7722854c9dd9cca294e7c7549fb26dce787
Bfacter <bhawanshbaleja@gmail.com>
2024-03-28 10:51:27 +0000

    [LeetCode Sync] Runtime - 7 ms (97.06%), Memory - 57.2 MB (18.62%)

--- my-folder/0131-palindrome-partitioning/solution.java
+++ my-folder/0131-palindrome-partitioning/solution.java
@@ -2,36 +2,36 @@ class Solution {
     public List<List<String>> partition(String s) {
         List<String> a = new ArrayList<>();
 		List<List<String>> b = new ArrayList<>();
-		Partition(s,a,b);
+		parti(s,0,a,b);
 		return b;
 
     }
-    public static void Partition(String ques,List<String> a,List<List<String>> b) {
-		if(ques.length()==0) {
-		
-			b.add(new ArrayList<>(a));
+   static void parti(String s, int i, List<String> a, List<List<String>> b) {
+        if (i == s.length()) {
+            b.add(new ArrayList<>(a));
             return;
-		}
-		
-		for(int cut=1;cut<=ques.length();cut++) {
-			String s=ques.substring(0,cut);
-			if(Palindrome(ques.substring(0,cut))==true) {
-				a.add(s);
-			Partition(ques.substring(cut),a,b);
-			a.remove(a.size()-1);}
-		}
-	}
-	 public static boolean Palindrome(String str){
-	        int i=0;
-	        int j=str.length()-1;
-	        while(i<j){
-	            if(str.charAt(i)!=str.charAt(j)){
-	                return false;
-	            }
-	            i++;
-	            j--;
-	        }
-	        return true;
-	    }
+        }
 
+        for (int j = i; j < s.length(); j++) {
+            String x = s.substring(i, j + 1);
+            if (Palindrome(x)) {
+                a.add(x);
+                parti(s, j + 1, a, b);
+                a.remove(a.size() - 1);
+            }
+        }
+    }
+
+    public static boolean Palindrome(String str) {
+        int i = 0;
+        int j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
 }
